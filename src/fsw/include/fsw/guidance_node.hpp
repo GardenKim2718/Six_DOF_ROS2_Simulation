4fa9b19856fc6aec3112f97f2542cc493229356f
GardenKim2718 <gardenkim2718@gmail.com>
2026-02-26 01:21:39 +0900

    updated guidance node : still WIP

---
+++ src/fsw/include/fsw/guidance_node.hpp
@@ -0,0 +1,133 @@
+/**
+ * @copyright KAIST, Department of Aerospace Engineering, 2026. All rights reserved.
+ *            Subject to limited distribution and restricted disclosure only.
+ *
+ * @file      guidance_node.hpp
+ * @brief     6-DOF guidance node header file
+ *
+ * @date      2026-02-09 created by Chungwon Kim (gardenkim@kaist.ac.kr)
+ *            2026-02-13 expanded by Chungwon Kim for 6-DOF control
+ */
+#ifndef __guidance_node_hpp__
+#define __guidance_node_hpp__
+
+#include <rclcpp/rclcpp.hpp>
+#include <cmath>
+#include <Eigen/Dense>
+#include <vector>
+#include <string>
+#include <mutex>
+#include <chrono>
+
+#include "interfaces/msg/state.hpp"
+#include "interfaces/msg/guidance.hpp"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+class Guidance : public rclcpp::Node
+{
+public:
+  Guidance();
+  ~Guidance();
+
+  void Init(const interfaces::msg::State& initial_state);
+  void Run();
+  void GetParameters();
+
+  private:
+    // add your member functions and variables here
+
+    // Callback function for command subscription
+    inline void CallbackState(
+        const interfaces::msg::State::SharedPtr msg)
+    {
+        std::lock_guard<std::mutex> lock(mutex_state_);
+        last_state_ = *msg;
+        b_simulator_initialized_ = true;
+    }
+
+    // Custom Functions
+    inline Eigen::Quaterniond QuaternionConjugate(
+        const Eigen::Quaterniond& q)
+    {
+        // return conjugate of quaternion
+        return Eigen::Quaterniond(q.w(), -q.x(), -q.y(), -q.z());
+    }
+
+    inline Eigen::Quaterniond QuaternionSignCorrection(
+        const Eigen::Quaterniond& q)
+    {
+        // Ensure that the quaternion scalar part is non-negative
+        if (q.w() < 0.0)
+        {
+            return Eigen::Quaterniond(-q.w(), -q.x(), -q.y(), -q.z());
+        } else {
+            return q;
+        }    
+    }
+
+    // topics
+    rclcpp::Publisher<interfaces::msg::Command>::SharedPtr pub_guidance_;
+    rclcpp::Subscription<interfaces::msg::State>::SharedPtr sub_state_;  
+
+    // mutex
+    std::mutex mutex_state_;
+
+    // Steady clock
+    rclcpp::Clock steady_clock{RCL_STEADY_TIME};
+
+    // timer
+    rclcpp::TimerBase::SharedPtr t_run_node_;
+
+    // input
+    interfaces::msg::State last_state_;
+
+    // output
+    interfaces::msg::Command o_guidance_;
+
+    // time
+    double time_prev_{0.0};
+
+    // loop rate
+    double loop_rate_hz_{20.0};
+
+    // target state
+    double target_x_ = 0.0;
+    double target_y_ = 0.0;
+    double target_z_ = 0.0;
+    double target_qx_ = 0.0;
+    double target_qy_ = 0.0;
+    double target_qz_ = 0.0;
+    double target_qw_ = 1.0;
+
+    // dynamic parameters
+    double mass_ = 1.0;
+    Eigen::Matrix3d inertia_ = Eigen::Matrix3d::Identity();
+    Eigen::Matrix3d inertia_inv_ = Eigen::Matrix3d::Identity();
+
+    // hardware limits
+    double max_force_ = 10.0;    // maximum force [N]
+    double max_torque_ = 10.0;   // maximum torque [N*m]
+
+    // declare the additional variables for yourself
+    bool b_simulator_initialized_ = false;
+
+    rclcpp::Time sim_time_prev_;
+    rclcpp::Time sim_time_curr_;
+
+    double time_dt_ = 0.0;
+
+    Eigen::Vector3d err_pos_ = Eigen::Vector3d::Zero();
+    Eigen::Vector3d err_vel_ = Eigen::Vector3d::Zero();
+    Eigen::Vector3d prev_err_pos_ = Eigen::Vector3d::Zero();
+    Eigen::Vector3d integral_err_pos_ = Eigen::Vector3d::Zero();
+
+    Eigen::Quaterniond err_quat_ = Eigen::Quaterniond::Identity();
+    Eigen::Quaterniond prev_err_quat_ = Eigen::Quaterniond::Identity();
+    Eigen::Vector3d err_ang_vel_ = Eigen::Vector3d::Zero();
+    Eigen::Vector3d integral_err_quat_ = Eigen::Vector3d::Zero();   // error quaternion vector part integration
+};
+
+#endif  // __guidance_node_hpp__
---
+++ src/fsw/src/guidance_node.cpp
@@ -0,0 +1,270 @@
+/**
+ * @copyright KAIST, Department of Aerospace Engineering, 2026. All rights reserved.
+ *            Subject to limited distribution and restricted disclosure only.
+ *
+ * @file      guidance_node.cpp
+ * @brief     6-DOF guidance node source file
+ *
+ * @date      2026-02-09 created by Chungwon Kim (gardenkim@kaist.ac.kr)
+ *            2026-02-13 expanded by Chungwon Kim for 6-DOF guidance
+ */
+
+#include "fsw/guidance_node.hpp"
+
+Guidance::Guidance()
+: Node("guidance_node")
+{
+    RCLCPP_INFO(this->get_logger(), "Initialize Guidance node...");
+    
+    //QoS settings
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+    
+    // Declare Parameters
+    this->declare_parameter("loop_rate_hz", loop_rate_hz_);
+    
+    this->declare_parameter("linear_kp", linear_kp_);
+    this->declare_parameter("linear_kd", linear_kd_);
+    this->declare_parameter("linear_ki", linear_ki_);
+    this->declare_parameter("angular_kp", angular_kp_);
+    this->declare_parameter("angular_kd", angular_kd_);
+    this->declare_parameter("angular_ki", angular_ki_);
+
+    this->declare_parameter("mass", mass_);
+
+    this->declare_parameter("inertia_xx", 1.0);
+    this->declare_parameter("inertia_yy", 1.0);
+    this->declare_parameter("inertia_zz", 1.0);
+    this->declare_parameter("inertia_xy", 0.0);
+    this->declare_parameter("inertia_xz", 0.0);
+    this->declare_parameter("inertia_yz", 0.0);
+
+    this->declare_parameter("max_force", max_force_);
+    this->declare_parameter("max_torque", max_torque_);
+
+    // Get parameters
+    GetParameters();
+
+    RCLCPP_INFO(this->get_logger(),
+        "Control Node Parameters: loop_rate_hz=%.3f", loop_rate_hz_);
+    
+    RCLCPP_INFO(this->get_logger(),
+        "Linear control Gains: linear_kp=%.3f, linear_kd=%.3f, linear_ki=%.3f",
+        linear_kp_, linear_kd_, linear_ki_);
+    
+    RCLCPP_INFO(this->get_logger(),
+        "Angular control Gains: angular_kp=%.3f, angular_kd=%.3f, angular_ki=%.3f",
+        angular_kp_, angular_kd_, angular_ki_);
+
+    // Subscribers Initialization
+    sub_state_ = this->create_subscription<interfaces::msg::State>(
+        "state", qos_profile,
+        std::bind(&Guidance::CallbackState, this, std::placeholders::_1));
+
+    sub_guidance_ = this->create_subscription<interfaces::msg::Guidance>(
+        "guidance", qos_profile,
+        std::bind(&Guidance::CallbackGuidance, this, std::placeholders::_1));
+
+    // Publishers Initialization
+    pub_guidance_ = this->create_publisher<interfaces::msg::Command>(
+        "command", qos_profile);
+    
+    // Timer Initialization
+    rclcpp::Time current_time = steady_clock.now();
+
+    // Run Contol Loop
+    t_run_node_ = this->create_wall_timer(
+        std::chrono::milliseconds((int64_t)(1000 / loop_rate_hz_)),
+        [this]() { this->Run(); });
+}
+
+Guidance::~Guidance()
+{
+    RCLCPP_INFO(this->get_logger(), "Shutting down Guidance node...");
+}
+
+void Guidance::GetParameters()
+{
+    // fetch parameters and store them in member variables
+    this->get_parameter("loop_rate_hz", loop_rate_hz_);
+    this->get_parameter("linear_kp", linear_kp_);
+    this->get_parameter("linear_kd", linear_kd_);
+    this->get_parameter("linear_ki", linear_ki_);
+    this->get_parameter("angular_kp", angular_kp_);
+    this->get_parameter("angular_kd", angular_kd_);
+    this->get_parameter("angular_ki", angular_ki_);
+
+    this->get_parameter("mass", mass_);
+
+    double Ixx, Iyy, Izz, Ixy, Ixz, Iyz;
+    this->get_parameter("inertia_xx", Ixx);
+    this->get_parameter("inertia_yy", Iyy);
+    this->get_parameter("inertia_zz", Izz);
+    this->get_parameter("inertia_xy", Ixy);
+    this->get_parameter("inertia_xz", Ixz);
+    this->get_parameter("inertia_yz", Iyz);
+
+    this->get_parameter("max_force", max_force_);
+    this->get_parameter("max_torque", max_torque_);
+
+    // fill in the MOI matrix
+    inertia_ << Ixx, Ixy, Ixz,
+                Ixy, Iyy, Iyz,
+                Ixz, Iyz, Izz;
+    
+    // compute inverse MOI matrix
+    inertia_inv_ = inertia_.inverse();
+}
+
+void Guidance::Init(const interfaces::msg::State& initial_state)
+{
+    // Log
+    RCLCPP_INFO(this->get_logger(),
+        "Starting Guidance Node Loop with loop_rate_hz=%.3f", loop_rate_hz_);
+
+    // Initialize time
+    sim_time_prev_ = initial_state.header.stamp;
+
+    // Initialize command message
+    o_guidance_.id = initial_state.id;
+    o_guidance_.force.x = 0.0;
+    o_guidance_.force.y = 0.0;
+    o_guidance_.force.z = 0.0;
+    o_guidance_.torque.x = 0.0;
+    o_command_.torque.y = 0.0;
+    o_command_.torque.z = 0.0;
+}
+
+void Control::Run()
+{
+    // handle initialization
+    if (!b_simulator_initialized_ || !b_guidance_initialized_) {
+        RCLCPP_WARN(this->get_logger(),
+            "Waiting for simulator and guidance initialization...");
+        return;
+    }
+
+    if (!b_control_initialized_) {
+        Init(last_state_);
+        b_control_initialized_ = true;
+    }
+
+    // get subscribed state
+    interfaces::msg::State current_state;
+    {
+        std::lock_guard<std::mutex> lock(mutex_state_);
+        current_state = last_state_;
+        sim_time_curr_ = current_state.header.stamp;
+    }
+
+    // get subscribed guidance
+    interfaces::msg::Guidance current_guidance;
+    {
+        std::lock_guard<std::mutex> lock(mutex_state_);
+        current_guidance = last_guidance_;
+    }
+
+    // compute time step
+    time_dt_ = (sim_time_curr_ - sim_time_prev_).seconds();
+    sim_time_prev_ = sim_time_curr_;
+
+    // attitude control
+    Eigen::Quaterniond q_current(
+        current_state.pose.orientation.w,
+        current_state.pose.orientation.x,
+        current_state.pose.orientation.y,
+        current_state.pose.orientation.z);
+    q_current.normalize();
+
+    Eigen::Vector3d w_current(
+        current_state.vel.angular.x,
+        current_state.vel.angular.y,
+        current_state.vel.angular.z);
+
+    Eigen::Quaterniond q_desired(
+        current_guidance.pose.orientation.w,
+        current_guidance.pose.orientation.x,
+        current_guidance.pose.orientation.y,
+        current_guidance.pose.orientation.z);
+    q_desired.normalize();
+
+    Eigen::Vector3d w_desired(
+        current_guidance.vel.angular.x,
+        current_guidance.vel.angular.y,
+        current_guidance.vel.angular.z);
+
+    err_quat_ = QuaternionSignCorrection(q_desired * QuaternionConjugate(q_current));
+    Eigen::Vector3d err_quat_vec(
+        err_quat_.x(), err_quat_.y(), err_quat_.z());
+    
+    err_ang_vel_ = w_desired - w_current;
+    integral_err_quat_ += Eigen::Vector3d(
+        err_quat_.x(), err_quat_.y(), err_quat_.z()) * time_dt_;
+    
+    Eigen::Vector3d torque_command;
+    torque_command = angular_kp_ * err_quat_vec
+                     + angular_kd_ * err_ang_vel_
+                     + angular_ki_ * integral_err_quat_
+                     - w_current.cross(inertia_ * w_current);
+
+    if (torque_command.norm() > max_torque_) {
+        torque_command = (torque_command.normalized()) * max_torque_;
+    }
+
+    // linear control
+    Eigen::Vector3d pos_current(
+        current_state.pose.position.x,
+        current_state.pose.position.y,
+        current_state.pose.position.z);
+    
+    Eigen::Vector3d speed_current(
+        current_state.vel.linear.x,
+        current_state.vel.linear.y,
+        current_state.vel.linear.z);
+    
+    Eigen::Vector3d pos_desired(
+        current_guidance.pose.position.x,
+        current_guidance.pose.position.y,
+        current_guidance.pose.position.z);
+    
+    Eigen::Vector3d speed_desired(
+        current_guidance.vel.linear.x,
+        current_guidance.vel.linear.y,
+        current_guidance.vel.linear.z);
+
+    err_pos_ = pos_desired - pos_current;
+    err_vel_ = speed_desired - speed_current;
+    integral_err_pos_ += err_pos_ * time_dt_;
+
+    Eigen::Vector3d acc_command_inertial;
+    acc_command_inertial = linear_kp_ * err_pos_
+                          + linear_kd_ * err_vel_
+                          + linear_ki_ * integral_err_pos_;
+    
+    Eigen::Vector3d acc_command_body;
+    Eigen::Matrix3d D_I2B = q_current.toRotationMatrix();
+    acc_command_body = D_I2B * acc_command_inertial;
+    Eigen::Vector3d force_command_body = mass_ * acc_command_body;
+
+    if (force_command_body.norm() > max_force_) {
+        force_command_body = (force_command_body.normalized()) * max_force_;
+    }
+
+    // publish command
+    o_command_.id = current_state.id;
+    o_command_.force.x = force_command_body.x();
+    o_command_.force.y = force_command_body.y();
+    o_command_.force.z = force_command_body.z();
+    o_command_.torque.x = torque_command.x();
+    o_command_.torque.y = torque_command.y();
+    o_command_.torque.z = torque_command.z();
+    pub_command_->publish(o_command_);
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Control>());
+
+  rclcpp::shutdown();
+  return 0;
+}
