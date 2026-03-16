ee4b6fffa9671f8131c75896c2222fc35080068a
GardenKim2718 <gardenkim2718@gmail.com>
2026-03-16 15:55:42 +0900

    minor fixes in fsw package and visualization update within RViz2

--- launch.sh
+++ launch.sh
@@ -2,15 +2,12 @@
 
 # Launch evaluator node in a new terminator window
 terminator -e "bash -c 'source ~/.bashrc; source install/setup.bash; ros2 launch evaluation evaluation.launch.xml; echo \"Press Enter to close the terminal...\"; read'" &
-sleep 0.05
 
 # Launch simulator node in a new terminator window
 terminator -e "bash -c 'source ~/.bashrc; source install/setup.bash; ros2 launch simulation simulator.launch.xml; echo \"Press Enter to close the terminal...\"; read'" &
-sleep 0.05
 
 # Launch control node in another window
 terminator -e "bash -c 'source ~/.bashrc; source install/setup.bash; ros2 launch fsw fsw.launch.xml; echo \"Press Enter to close the terminal...\"; read'" &
-sleep 0.05
 
 # Monitor State in another window
 # terminator -e "bash -c 'source ~/.bashrc; source install/setup.bash; ros2 topic echo /evaluation; echo \"Press Enter to close the terminal...\"; read'"
